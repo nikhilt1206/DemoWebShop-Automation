1dbcf275f2238c6eba0da96fe19c4df88efd582e
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-07-03 00:17:49 +0530

    Added Billing Address step verification in checkout flow

--- src/test/java/com/nikhil/automation/pages/CheckoutPage.java
+++ src/test/java/com/nikhil/automation/pages/CheckoutPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 public class CheckoutPage extends BasePage{
 
     private By checkoutHeader = By.cssSelector(".page-title h1");
+    private By billingAddressContinueButton = By.cssSelector(".new-address-next-step-button");
+    private By inStorePickupCheckbox = By.id("PickUpInStore");
 
     public CheckoutPage(WebDriver driver){
         super(driver);
@@ -14,5 +16,14 @@ public class CheckoutPage extends BasePage{
     public boolean isCheckoutPageDisplayed(){
         return isDisplayed(checkoutHeader);
     }
+
+    public void clickBillingAddressContinue(){
+        click(billingAddressContinueButton);
+    }
+
+    public boolean isInStorePickupCheckboxDisplayed(){
+        return isDisplayed(inStorePickupCheckbox);
+    }
+
 
 }
--- src/test/java/com/nikhil/automation/tests/CheckoutTest.java
+++ src/test/java/com/nikhil/automation/tests/CheckoutTest.java
@@ -27,4 +27,24 @@
             CheckoutPage checkoutPage = new CheckoutPage(driver);
             Assert.assertTrue(checkoutPage.isCheckoutPageDisplayed());
         }
+
+        @Test
+        public void verifyRegisteredUserCanProceedToShippingAddress(){
+            HomePage homePage = new HomePage(driver);
+            LoginPage loginPage = homePage.clickLogin();
+            AccountPage accountPage = loginPage.login(ConfigReader.getProperty("email"),
+                    ConfigReader.getProperty("password"));
+            HeaderComponent headerComponent = accountPage.getHeader();
+            SearchResultsPage searchResultsPage = headerComponent.searchProduct("Laptop");
+            ProductPage productPage = searchResultsPage.clickProduct();
+            productPage.addToCart();
+            headerComponent = productPage.getHeader();
+            headerComponent.waitForCartCount("(1)");
+            CartPage cartPage = headerComponent.clickShoppingCart();
+            cartPage.acceptTermsAndConditions();
+            cartPage.clickCheckout();
+            CheckoutPage checkoutPage = new CheckoutPage(driver);
+            checkoutPage.clickBillingAddressContinue();
+            Assert.assertTrue(checkoutPage.isInStorePickupCheckboxDisplayed());
+        }
     }
