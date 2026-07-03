8f8b8e8dd938a80f793d070fa648985042f1c5a0
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-07-04 00:22:51 +0530

    Implemented Shipping Method navigation in checkout workflow

--- src/test/java/com/nikhil/automation/pages/CheckoutPage.java
+++ src/test/java/com/nikhil/automation/pages/CheckoutPage.java
@@ -3,27 +3,35 @@ package com.nikhil.automation.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class CheckoutPage extends BasePage{
+public class CheckoutPage extends BasePage {
 
     private By checkoutHeader = By.cssSelector(".page-title h1");
     private By billingAddressContinueButton = By.cssSelector(".new-address-next-step-button");
     private By inStorePickupCheckbox = By.id("PickUpInStore");
+    private By shippingAddressContinueButton = By.cssSelector("input[onclick='Shipping.save()']");
+    private By groundShippingMethodRadioButton = By.id("shippingoption_0");
 
-    public CheckoutPage(WebDriver driver){
+    public CheckoutPage(WebDriver driver) {
         super(driver);
     }
 
-    public boolean isCheckoutPageDisplayed(){
+    public boolean isCheckoutPageDisplayed() {
         return isDisplayed(checkoutHeader);
     }
 
-    public void clickBillingAddressContinue(){
+    public void clickBillingAddressContinue() {
         click(billingAddressContinueButton);
     }
 
-    public boolean isInStorePickupCheckboxDisplayed(){
+    public boolean isInStorePickupCheckboxDisplayed() {
         return isDisplayed(inStorePickupCheckbox);
     }
 
+    public void clickShippingAddressContinueBtn() {
+        click(shippingAddressContinueButton);
+    }
 
+    public boolean isGroundShippingMethodDisplayed() {
+        return isDisplayed(groundShippingMethodRadioButton);
+    }
 }
--- src/test/java/com/nikhil/automation/tests/CheckoutTest.java
+++ src/test/java/com/nikhil/automation/tests/CheckoutTest.java
@@ -47,4 +47,25 @@
             checkoutPage.clickBillingAddressContinue();
             Assert.assertTrue(checkoutPage.isInStorePickupCheckboxDisplayed());
         }
+
+        @Test
+        public void verifyRegisteredUserCanProceedToShippingMethod(){
+            HomePage homePage = new HomePage(driver);
+            LoginPage loginPage = homePage.clickLogin();
+            AccountPage accountPage = loginPage.login(ConfigReader.getProperty("email"),
+                    ConfigReader.getProperty("password"));
+            HeaderComponent headerComponent = accountPage.getHeader();
+            SearchResultsPage searchResultsPage = headerComponent.searchProduct("Laptop");
+            ProductPage productPage = searchResultsPage.clickProduct();
+            productPage.addToCart();
+            headerComponent = productPage.getHeader();
+            headerComponent.waitForCartCount("(1)");
+            CartPage cartPage = headerComponent.clickShoppingCart();
+            cartPage.acceptTermsAndConditions();
+            cartPage.clickCheckout();
+            CheckoutPage checkoutPage = new CheckoutPage(driver);
+            checkoutPage.clickBillingAddressContinue();
+            checkoutPage.clickShippingAddressContinueBtn();
+            Assert.assertTrue(checkoutPage.isGroundShippingMethodDisplayed());
+        }
     }
