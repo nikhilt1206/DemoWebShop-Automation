712b1eafc95d61e2b39cd44cc1874dbfe1248fc5
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-07-15 00:39:06 +0530

    feat: add OrderConfirmationPage page object

---
+++ src/test/java/com/nikhil/automation/pages/OrderConfirmationPage.java
@@ -0,0 +1,28 @@
+package com.nikhil.automation.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class OrderConfirmationPage extends BasePage {
+
+    private By successMessage=By.cssSelector(".section.order-completed .title");
+    private By orderNumber=By.cssSelector(".details li");
+    private By continueButton=By.cssSelector(".order-completed-continue-button");
+
+    public OrderConfirmationPage(WebDriver driver){
+        super(driver);
+    }
+
+    public boolean isOrderPlaced(){
+        return isDisplayed(successMessage);
+    }
+
+    public String getOrderNumber(){
+        return getText(orderNumber);
+    }
+
+    public HomePage clickContinue(){
+        click(continueButton);
+        return new HomePage(driver);
+    }
+}
