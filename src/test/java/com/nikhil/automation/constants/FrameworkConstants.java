f963d7a398db9c31059a925f20d04dcb8a1fad80
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-07-26 22:32:16 +0530

    Add ExtentReports manager and reporting configuration

--- src/test/java/com/nikhil/automation/constants/FrameworkConstants.java
+++ src/test/java/com/nikhil/automation/constants/FrameworkConstants.java
@@ -1,5 +1,10 @@
 package com.nikhil.automation.constants;
 
-public class FrameworkConstants {
+public final class FrameworkConstants {
+    private FrameworkConstants(){
+    }
     public static final int EXPLICIT_WAIT=10;
+    public static final String REPORT_PATH="test-output/ExtentReport.html";
+    public static final String REPORT_NAME="DemoWebShop Automation Report";
+    public static final String REPORT_TITLE="DemoWebShop Report";
 }
--- src/test/java/com/nikhil/automation/reporting/ExtentManager.java
+++ src/test/java/com/nikhil/automation/reporting/ExtentManager.java
@@ -1,4 +1,22 @@
 package com.nikhil.automation.reporting;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.nikhil.automation.constants.FrameworkConstants;
+
 public class ExtentManager {
+    private static ExtentReports extentReports;
+
+    public static ExtentReports getExtentReports(){
+        if(extentReports==null){
+            extentReports = new ExtentReports();
+            ExtentSparkReporter extentSparkReporter = new ExtentSparkReporter(FrameworkConstants.REPORT_PATH);
+            extentSparkReporter.config().setReportName(FrameworkConstants.REPORT_NAME);
+            extentSparkReporter.config().setDocumentTitle(FrameworkConstants.REPORT_TITLE   );
+            extentSparkReporter.config().setTheme(Theme.DARK);
+            extentReports.attachReporter(extentSparkReporter);
+        }
+       return extentReports;
+    }
 }
